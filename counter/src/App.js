3323593fef4897728ef81d98c1ca3373d38a75a6
Safwan <safwanmv007@gmail.com>
2025-05-01 13:05:12 +0530

    fix: corrected button functionality and adjusted event handler name consistency

--- counter/src/App.js
+++ counter/src/App.js
@@ -1,15 +1,24 @@
 import { useState } from 'react';
+
 import './App.css';
+import CounterButton from './components/CounterButton';
+
+
 
 function App() {
-  const [count,setCounter]=useState(0)
+  const [count, setCounter ]=useState(0)
+
+  const minusCount=()=>setCounter(count - 1);
+  const resetCount=()=>setCounter(0);
+  const addCount=()=>setCounter(count + 1);
+
   return (
     <div className="App">
       <div className="section">
         <h1 className='cnt'>Counter: {count} </h1>
-        <counterButton onClick={minusCount}>-</counterButton>
-        <counterButton onClick={resetCount}>RESET</counterButton>
-        <counterButton onclick={addCount}>+</counterButton>
+        <CounterButton onClick={minusCount}>-</CounterButton>
+        <CounterButton onClick={resetCount}>RESET</CounterButton>
+        <CounterButton onClick={addCount}>+</CounterButton>
       </div>
     </div>
   );
--- counter/src/components/counterButton.js
+++ counter/src/components/counterButton.js
@@ -1,10 +1,10 @@
-import React, { Children }  from "react";
+import React from "react";
 import './counterButton.css';
 
-const counterButton=({onclick,Children})=>{
-    return(
-        <button className="btn" onClick={onclick}> {Children} </button>
-    )
-}
+const CounterButton= ({ onClick , children }) => {
+    return (
+        <button className="btn" onClick={onClick}> { children} </button>
+    );
+};
 
-export default counterButton
+export default CounterButton;
